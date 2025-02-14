74b8a442682aa853ded98bfe9459bf99d061a509
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-02-15 00:46:13 +0900

    fix

--- src/components/atoms/InputForm/InputForm.jsx
+++ src/components/atoms/InputForm/InputForm.jsx
@@ -1,11 +1,18 @@
 import styles from "./style.module.css";
 
-export const InputForm = ({ inputValue, placeholder, onChange, onKeyDown }) => (
+export const InputForm = ({
+  disabled = false,
+  value,
+  placeholder,
+  onChange,
+  onKeyDown,
+}) => (
   <input
+    disabled={disabled}
     className={styles.input}
     type="text"
     placeholder={placeholder}
-    value={inputValue}
+    value={value}
     onChange={onChange}
     onKeyDown={onKeyDown}
   />
