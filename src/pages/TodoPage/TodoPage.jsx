af360dcd795816a4234b62b13d1b8eb6201b275d
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-02-15 00:08:05 +0900

    pages

--- src/pages/TodoPage/TodoPage.jsx
+++
@@ -1,9 +0,0 @@
-import { TodoProvider } from "../../contexts/TodoContext";
-import { TodoTemplate } from "../../components/templates";
-
-export const TodoPage = () => (
-  <TodoProvider>
-    {/* TodoProvider直下のコンポーネントでコンテキストに定義した状態、ロジックをどこでも呼び出せる */}
-    <TodoTemplate />
-  </TodoProvider>
-);
